e028af7a25f2f379efcc7ed5491adbd95f617d4c
m3nnoun <moulshe@protonmail.com>
2025-09-30 19:24:08 +0100

    building the custum logging for the project

---
+++ src/logging/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import logging
+
+logging_str="[%(asctime)s - %(levelname)s - %(module)s - %(message)s]"
+
+log_dir="logs"
+
+log_file_path=os.path.join(log_dir,"logs.log")
+
+os.makedirs(log_dir,exist_ok=True)
+
+
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.INFO,
+    format=logging_str,
+    filemode="a",
+    handlers=[
+        logging.FileHandler(log_file_path),
+    ]
+)
+
+logger=logging.getLogger()
